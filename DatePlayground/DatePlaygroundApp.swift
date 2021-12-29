669da2e6b3d6d8e6670a36be80dc0e4adaa13ee3
ot <o@o>
2021-12-29 10:37:17 -0500

    Initial Commit

---
+++ DatePlayground/DatePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatePlaygroundApp.swift
+//  DatePlayground
+//
+//  Created by ot on 29.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct DatePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
