b92ffdf53978fc9161e04a428093f853e9107667
ot <o@o>
2021-12-30 14:45:53 -0500

    basic date calculator for adding and subtracting days from give base date

--- DatePlayground/ContentView.swift
+++ DatePlayground/ContentView.swift
@@ -8,10 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
+    @State var days:Int=0
+    @State var calculations:[String]=[]
+    @State private var date = Date()
+
+    static var decimal: NumberFormatter {
+            let formatter = NumberFormatter()
+            formatter.numberStyle = .decimal
+            return formatter
     }
+    var body: some View {
+        VStack{
+            HStack {
+                Button("Reset"){calculations=[]}
+            }.frame(alignment:.trailing)
+            DatePicker(selection: $date, label: { Text("Now") })
+            
+            HStack {
+                Text("Add/Subtract Days:")
+                TextField("How many days",value: $days,format:.number).onSubmit {
+                    print("submitted \(days)")
+                    let days_ago = Date(timeIntervalSince1970: date.timeIntervalSince1970+Double(days*24*60*60))
+                    
+                    calculations.insert("\(days) ago is \(days_ago)", at:0)
+                }
+                
+            }
+            ScrollView {
+                ForEach(calculations, id: \.self) { row in
+                    Text(row).font(.title).foregroundColor(.blue).bold()
+                }
+            }.padding(.top)
+            Spacer()
+        }.frame(minWidth:200, minHeight:400).padding()
+        
+    };
 }
 
 struct ContentView_Previews: PreviewProvider {
